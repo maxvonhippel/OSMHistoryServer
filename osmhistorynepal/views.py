2540c2786dafa691cd78c10d91cbf9629c1f2cc9
Max von Hippel <maxvonhippel1996@gmail.com>
2016-11-17 16:42:02 +0545

    added debugging tool I wrote, hopefully it works

--- osmhistorynepal/views.py
+++ osmhistorynepal/views.py
@@ -15,6 +15,23 @@ import dateutil.parser
 from django.db import connection
 import time
 
+# debug tool for query speed analysis
+class debug_tool:
+	prints = 0
+	def __init__(self):
+		self.start = time.now()
+		printstatement = "debug tool instantiated: " + self.start
+		print(printstatement)
+	def print(self, msg)
+		prints += 1
+		printstatement = time.now() + " ->> Debug statement #" + prints + "\noutput:" + msg
+		print(printstatement)
+	def end(self)
+		elap = time.now() - self.start
+		printstatement = prints + " statements printed, " + elap + " seconds elapsed since function start"
+		print(printstatement)
+
+
 # returns a javascript formatted array of usernames for all of nepal
 def user_names_view(request):
 	c = connection.cursor()
@@ -35,9 +52,9 @@ def nepal_statistics_view(request):
 	# make our json obj
 	nstat = {}
 	# count the distinct mappers
-	
+
 	# --- this could be significantly sped up by combining queries w/ aggregate, but it's not worth working on rn ----
-	
+
 	nstat['mappers'] = ob.values('uid').distinct().count()
 	# count the distinct buildings
 	nstat['buildings'] = ob.filter(tags__contains=['building']).values( \
@@ -55,12 +72,12 @@ def nepal_statistics_view(request):
         	).values('feature_type','feature_id').count()
 	# wrap it up in a json format and return it
 	return JsonResponse(nstat)
- 
-# returns a json array with data on a specific area of nepal in a specific time range, optionally with extra info for a specific user   
+
+# returns a json array with data on a specific area of nepal in a specific time range, optionally with extra info for a specific user
 def selection_statistics_view(request, range, mn_x, mn_y, mx_x, mx_y, user):
-	
-	starttime = time.time() # debug
-	
+
+	d = debug_tool() # DEBUG
+
 	# parse range
 	sstart,send = range.split(",") # 2007-08-29T04:08:07+05:45,2007-08-29T04:08:07+05:45
 	start = dateutil.parser.parse(sstart)
@@ -70,15 +87,12 @@ def selection_statistics_view(request, range, mn_x, mn_y, mx_x, mx_y, user):
 	# get all the objects
 	ndtmp = Feature.geoobjects.filter(Q(point__intersects=box) & Q(feature_type='node'))
 	# get the unique ids from ndtmp as strings
-	strids = ndtmp.extra({'feature_id_str':"CAST(feature_id AS VARCHAR)"}).values_list('feature_id_str',flat=True).distinct()	
+	strids = ndtmp.extra({'feature_id_str':"CAST(feature_id AS VARCHAR)"}).values_list('feature_id_str',flat=True).distinct()
 	# combine all features containing >=1 ok members with my existing list of ok nodes
 	ob = Feature.geoobjects.filter(members__ref__in=strids) | ndtmp
-	# for more, see: 
+	# for more, see:
 	# http://stackoverflow.com/questions/40585055/querying-objects-using-attribute-of-member-of-many-to-many/40602515#40602515
-	
-	endobtime = time.time()
-	print "finished ob component in %f" %(endobtime - starttime) # debug
-	# ------------------------------------------------------------------------------------------------------------------
+
 	selection = ob.filter(timestamp__lte=end).values('feature_type','feature_id').aggregate( \
 		Buildings_start=Sum( \
 			Case(When(timestamp__date__lte=start, tags__contains=['building'], then = 1), \
@@ -102,7 +116,7 @@ def selection_statistics_view(request, range, mn_x, mn_y, mx_x, mx_y, user):
         	Schools_start=Sum( \
         		Case(When((Q(tags__contains=['school']) | Q(tags__contains=['college']) | \
 			Q(tags__contains=['university']) | Q(tags__contains=['kindergarten']) | \
-			Q(tags__contains=['music_school']) & Q(timestamp__date__lte=start)), then = 1), \
+			Q(tags__contains=['music_school']) & Q(timestamp__date__lte=start), then = 1), \
 			default = 0,
 			output_field=IntegerField())), \
 		Schools_end=Sum( \
@@ -120,20 +134,14 @@ def selection_statistics_view(request, range, mn_x, mn_y, mx_x, mx_y, user):
 			default = 0, \
 			output_field=IntegerField())) \
 	)
-	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ start of selection statistics ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	
-	endselecttime = time.time()
-	print "finished select component in %f" %(endselecttime - endobtime)
+
+	c.print(selection) # DEBUG
 
 	# make our json obj
 	stat = {}
-	
+
 	stat['Selection Statistics'] = selection
-	
-	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ end of selection statistics ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	
-	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ start of leaderboard statistics ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	
+
 	# leaderboards
 	# ways
 	ws = ob.filter(Q(timestamp__date__range=[start,end]) & Q(feature_type='way')).values_list('user').annotate( \
@@ -141,46 +149,51 @@ def selection_statistics_view(request, range, mn_x, mn_y, mx_x, mx_y, user):
 	war = [ [ ("OSM Username",ws[0][0]), ("Ways",ws[0][1]), ("Rank","first") ], \
         	[ ("OSM Username",ws[1][0]), ("Ways", ws[1][1]), ("Rank","second") ], \
 		[ ("OSM Username", ws[2][0]), ("Ways", ws[1][1]), ("Rank","third")] ]
+
+	c.print(war)	# debug
+
 	# nodes
 	ns = ob.filter(Q(timestamp__date__range=[start,end]) & Q(feature_type='node')).values_list('user').annotate( \
         	num=Count('user')).order_by('-num')
 	nar = [ [ ("OSM Username", ns[0][0]), ("Nodes", ns[0][1]), ("Rank", "first") ], \
         	[ ("OSM Username", ns[1][0]), ("Nodes", ns[1][1]), ("Rank", "second") ], \
 		[ ("OSM Username", ns[2][0]), ("Noses", ns[1][1]), ("Rank", "third")] ]
+
+	c.print(nar)	# DEBUG
+
 	# put them in our stat object and find most freq. edited POI
 	foundnodes = False
 	foundways = False
 	pres = [ "first", "second", "third" ]
-	
-	endwartime = time.time()
-	print "finished war component in %f" %(endwartime - endselecttime)
-	
-	for index in range(len(pres)):
+
+	for index, word in enumerate(pres):
         	# Nodes
-		stat['Nodes'][pres[index]] = nar[index]
-		stat['Nodes'][pres[index]]['Most Frequently Edited POI'] = ob.filter(Q(user=nar[index][0][1]) & \
+		stat['Nodes'][word] = nar[index]
+		stat['Nodes'][word]['Most Frequently Edited POI'] = ob.filter(Q(user=nar[index][0][1]) & \
 			Q(timestamp__date__range=[start,end]) & Q(feature_type='node')).raw('''SELECT k, v, count(*) \
 			as count FROM ( SELECT skeys(tags) AS k, svals(tags) \
 			as v, user, timestamp FROM populate_feature) AS t \
 			WHERE k='amenity' GROUP BY k, v ORDER BY count DESC LIMIT 1''')
 		# http://stackoverflow.com/questions/12522966/django-orm-hstore-counting-unique-values-of-a-key
-		if user == stat['Nodes'][pres[index]]['OSM Username']:
-			stat['Nodes'][pres[index]]['highlighted'] = True
+		if user == stat['Nodes'][word]['OSM Username']:
+			stat['Nodes'][word]['highlighted'] = True
 			foundnodes = True
+
+		c.print(stat['Nodes'][word])	# DEBUG
+
 		# Ways
-		stat['Ways'][pres[index]] = war[index]
-		stat['Ways'][pres[index]]['Most Frequently Edited POI'] = ob.filter(Q(user=nar[index][0][1]) & \
+		stat['Ways'][word] = war[index]
+		stat['Ways'][word]['Most Frequently Edited POI'] = ob.filter(Q(user=nar[index][0][1]) & \
 			Q(timestamp__date__range=[start,end]) & Q(feature_type='way')).raw('''SELECT k, v, count(*) \
 			as count FROM ( SELECT skeys(tags) AS k, svals(tags) \
 			as v, user, timestamp FROM populate_feature) AS t \
 			WHERE k='amenity' GROUP BY k, v ORDER BY count DESC LIMIT 1''')
-		if user == stat['Ways'][pres[index]]['OSM Username']:
-			stat['Ways'][pres[index]]['highlighted'] = True
+		if user == stat['Ways'][word]['OSM Username']:
+			stat['Ways'][word]['highlighted'] = True
 			foundways = True
-			
-	endleadertime = time.time()
-	print "finished leader component in %f" %(endleadertime - endwartime)
-	
+
+		c.print(stat['Ways'][word])	# DEBUG
+
 	# user search nodes
 	if user != "" and not foundnodes:
         	stat['Nodes']['user']['OSM Username'] = user
@@ -188,27 +201,26 @@ def selection_statistics_view(request, range, mn_x, mn_y, mx_x, mx_y, user):
 		for index, item in enumerate(ns):
 			if item[index][0] == user:
 				stat['Nodes']['user']['rank'] = index
-				foundnr = True 
+				foundnr = True
 				break
 		if not foundnr:
-			stat['Nodes']['user']['rank'] = 0			
+			stat['Nodes']['user']['rank'] = 0
 		stat['Nodes']['user']['highlighted'] = True
 		stat['Nodes']['user']['Most Frequently edited POI'] = ob.filter(Q(user=user) & \
 		Q(timestamp__date__range=[start,end]) & Q(feature_type='node')).raw('''SELECT k, v, count(*) \
 			as count FROM ( SELECT skeys(tags) AS k, svals(tags) \
 			as v, user, timestamp FROM populate_feature) AS t \
 			WHERE k='amenity' GROUP BY k, v ORDER BY count DESC LIMIT 1''')
-			
-	endusernodetime = time.time()
-	print "finished user nodes component in %f" %(endusernodetime - endleadertime)
-	
+
+		c.print(stat['Nodes']['user'])	# DEBUG
+
 	# user search ways
 	if user != "" and not foundways:
         	stat['Ways']['user']['OSM Username'] = user
 		foundwr = False
 		for index, item in enumerate(ws):
 			if item[index][0] == user:
-				stat['Ways']['user']['rank'] = index 
+				stat['Ways']['user']['rank'] = index
 				foundwr = True
 				break
 		if not foundwr:
@@ -220,12 +232,11 @@ def selection_statistics_view(request, range, mn_x, mn_y, mx_x, mx_y, user):
 			as count FROM ( SELECT skeys(tags) AS k, svals(tags) \
 			as v, user, timestamp FROM populate_feature) AS t \
 			WHERE k='amenity' GROUP BY k, v ORDER BY count DESC LIMIT 1''')
-	
-	enduserwaytime = time.time()
-	print("finished user nodes component in " + (enduserwaytime - endusernodetime))	
-	
-	print("finished total view in " + (enduserwaytime - starttime))		
-	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ start of leaderboard statistics ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	
+
+		c.print(stat['Ways']['user'])	# DEBUG
+
+	c.print(stat)	# DEBUG
+	c.end()
+
 	# wrap it up in a json format
 	return JsonResponse(stat)
