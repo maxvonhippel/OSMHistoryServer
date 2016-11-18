06065d45ff24c15a6309754340d844ab2c7e3917
Max von Hippel <maxvonhippel1996@gmail.com>
2016-11-18 10:25:21 +0545

    fixed some more small typos and things and made the code smaller and faster all around, still need to debug though

--- osmhistorynepal/views.py
+++ osmhistorynepal/views.py
@@ -76,7 +76,7 @@ def nepal_statistics_view(request):
 			default = 0,
 			output_field = IntegerField())) )
 	# count the distinct mappers
-	nstat['mappers'] = ob.values('uid').distinct().count()
+	nstat['Mappers'] = ob.values('uid').distinct().count()
 	# wrap it up in a json format and return it
 	return JsonResponse(nstat)
 
@@ -101,43 +101,47 @@ def selection_statistics_view(request, range, mn_x, mn_y, mx_x, mx_y, user):
 	# http://stackoverflow.com/questions/40585055/querying-objects-using-attribute-of-member-of-many-to-many/40602515#40602515
 
 	selection = ob.filter(timestamp__lte=end).values('feature_type','feature_id').aggregate( \
-		Buildings_start=Sum( \
+		Buildings_start = Sum( \
 			Case(When(timestamp__date__lte=start, tags__contains=['building'], then = 1), \
 			default = 0, \
 			output_field=IntegerField())), \
-		Buildings_end=Sum( \
-			Case(When(tags__contains=['building'], then = 1), \
-			default = 0, \
-			output_field=IntegerField())), \
-		Roads_start=Sum( \
+		Roads_start = Sum( \
 			Case(When((Q(tags__contains={'bridge':'yes'}) | Q(tags__contains={'tunnel':'yes'}) | \
 			Q(tags__contains=['highway']) | Q(tags__contains=['tracktype'])) & \
 			Q(timestamp__date__lte=start), then = 1), \
-			default = 0,
+			default = 0, \
 			output_field=IntegerField())), \
-        	Roads_end=Sum( \
-			Case(When((Q(tags__contains={'bridge':'yes'}) | Q(tags__contains={'tunnel':'yes'}) | \
-			Q(tags__contains=['highway']) | Q(tags__contains=['tracktype'])), then = 1), \
-			default = 0,
-			output_field=IntegerField())), \
-        	Schools_start=Sum( \
+		Education_start = Sum( \
         		Case(When((Q(tags__contains=['school']) | Q(tags__contains=['college']) | \
 			Q(tags__contains=['university']) | Q(tags__contains=['kindergarten']) | \
 			Q(tags__contains=['music_school']) & Q(timestamp__date__lte=start)), then = 1), \
-			default = 0,
+			default = 0, \
 			output_field=IntegerField())), \
-		Schools_end=Sum( \
+		Health_start = Sum( \
+			Case(When((Q(tags__contains=['hospital']) | Q(tags__contains=['health']) | \
+			Q(tags__contains=['clinic']) | Q(tags__contains=['dentist']) | Q(tags__contains=['medical']) | \
+			Q(tags__contains=['surgery'])) & Q(timestamp__date__lte=start), then = 1), \
+			default = 0, \
+			output_field=IntegerField())), \
+		Buildings_end = Sum( \
+			Case(When(tags__contains=['building'], then = 1), \
+			default = 0, \
+			output_field=IntegerField())), \
+        	Roads_end = Sum( \
+			Case(When((Q(tags__contains={'bridge':'yes'}) | Q(tags__contains={'tunnel':'yes'}) | \
+			Q(tags__contains=['highway']) | Q(tags__contains=['tracktype'])), then = 1), \
+			default = 0, \
+			output_field=IntegerField())), \
+       		Education_end = Sum( \
 			Case(When((Q(tags__contains=['school']) | Q(tags__contains=['college']) | \
 			Q(tags__contains=['university']) | Q(tags__contains=['kindergarten']) | \
 			Q(tags__contains=['music_school'])), then = 1), \
-			default = 0,
-			output_field=IntegerField())), \
-		Hospitals_start=Sum( \
-			Case(When(tags__contains=['hospital'], timestamp__date__lte=start, then = 1), \
 			default = 0, \
 			output_field=IntegerField())), \
-		Hospitals_end=Sum( \
-			Case(When(tags__contains=['hospital'], then = 1), \
+		Health_end = Sum( \
+			Case(When((Q(tags__contains=['hospital']) | Q(tags__contains=['health']) | \
+			Q(tags__contains=['clinic']) | Q(tags__contains=['dentist']) | Q(tags__contains=['medical']) | \
+			Q(tags__contains=['surgery'])), then = 1), \
 			default = 0, \
 			output_field=IntegerField())) \
 	)
@@ -153,32 +157,26 @@ def selection_statistics_view(request, range, mn_x, mn_y, mx_x, mx_y, user):
 	# ways
 	ws = ob.filter(Q(timestamp__date__range=[start,end]) & Q(feature_type='way')).values_list('user').annotate( \
         	num=Count('user')).order_by('-num')
-	war = [ [ ("OSM Username",ws[0][0]), ("Ways",ws[0][1]), ("Rank","first") ], \
-        	[ ("OSM Username",ws[1][0]), ("Ways", ws[1][1]), ("Rank","second") ], \
-		[ ("OSM Username", ws[2][0]), ("Ways", ws[1][1]), ("Rank","third")] ]
-
-	d.deprint(war)	# debug
 
 	# nodes
 	ns = ob.filter(Q(timestamp__date__range=[start,end]) & Q(feature_type='node')).values_list('user').annotate( \
         	num=Count('user')).order_by('-num')
-	nar = [ [ ("OSM Username", ns[0][0]), ("Nodes", ns[0][1]), ("Rank", "first") ], \
-        	[ ("OSM Username", ns[1][0]), ("Nodes", ns[1][1]), ("Rank", "second") ], \
-		[ ("OSM Username", ns[2][0]), ("Noses", ns[1][1]), ("Rank", "third")] ]
-
-	d.deprint(nar)	# DEBUG
 
 	# put them in our stat object and find most freq. edited POI
 	foundnodes = False
 	foundways = False
-	pres = [ "first", "second", "third" ]
+
+	pres = [ "first", "second", "third", "fourth", "fifth" ]
 
 	stat['Nodes'] = {}
 	stat['Ways'] = {}
 
 	for index, word in enumerate(pres):
+
         	# Nodes
-		stat['Nodes'][word] = nar[index]
+		stat['Nodes'][word]["OSM Username"] = ns[index][0]
+		stat['Nodes'][word]["Nodes"] = ns[index][1]
+		stat['Nodes'][word]["Rank"] = index
 		stat['Nodes'][word]['Most Frequently Edited POI'] = ob.filter(Q(user=nar[index][0][1]) & \
 			Q(timestamp__date__range=[start,end]) & Q(feature_type='node')).raw('''SELECT k, v, count(*) \
 			as count FROM ( SELECT skeys(tags) AS k, svals(tags) \
@@ -186,64 +184,61 @@ def selection_statistics_view(request, range, mn_x, mn_y, mx_x, mx_y, user):
 			WHERE k='amenity' GROUP BY k, v ORDER BY count DESC LIMIT 1''')
 		# http://stackoverflow.com/questions/12522966/django-orm-hstore-counting-unique-values-of-a-key
 		if user == stat['Nodes'][word]['OSM Username']:
-			stat['Nodes'][word]['highlighted'] = True
+			stat['Nodes'][word]['highlighted'] = 1
 			foundnodes = True
 
 		d.deprint(stat['Nodes'][word])	# DEBUG
 
 		# Ways
-		stat['Ways'][word] = war[index]
+		stat['Ways'][word]['OSM Username'] = ws[index][0]
+		stat['Ways'][word]['Ways'] = ws[index][1]
+		stat['Ways'][word]['Rank'] = index
 		stat['Ways'][word]['Most Frequently Edited POI'] = ob.filter(Q(user=nar[index][0][1]) & \
 			Q(timestamp__date__range=[start,end]) & Q(feature_type='way')).raw('''SELECT k, v, count(*) \
 			as count FROM ( SELECT skeys(tags) AS k, svals(tags) \
 			as v, user, timestamp FROM populate_feature) AS t \
 			WHERE k='amenity' GROUP BY k, v ORDER BY count DESC LIMIT 1''')
 		if user == stat['Ways'][word]['OSM Username']:
-			stat['Ways'][word]['highlighted'] = True
+			stat['Ways'][word]['Highlighted'] = 1
 			foundways = True
 
 		d.deprint(stat['Ways'][word])	# DEBUG
 
 	# user search nodes
 	if user != "" and not foundnodes:
-        	stat['Nodes']['user']['OSM Username'] = user
+
 		foundnr = False
 		for index, item in enumerate(ns):
 			if item[index][0] == user:
-				stat['Nodes']['user']['rank'] = index
-				foundnr = True
-				break
-		if not foundnr:
-			stat['Nodes']['user']['rank'] = 0
-		stat['Nodes']['user']['highlighted'] = True
-		stat['Nodes']['user']['Most Frequently edited POI'] = ob.filter(Q(user=user) & \
-		Q(timestamp__date__range=[start,end]) & Q(feature_type='node')).raw('''SELECT k, v, count(*) \
-			as count FROM ( SELECT skeys(tags) AS k, svals(tags) \
-			as v, user, timestamp FROM populate_feature) AS t \
-			WHERE k='amenity' GROUP BY k, v ORDER BY count DESC LIMIT 1''')
+				stat['Nodes']['fifth'] = {}
+				stat['Nodes']['fifth']['Rank'] = index
+				stat['Nodes']['fifth']['OSM Username'] = user
+				stat['Nodes']['fifth']['Highlighted'] = 1
+				stat['Nodes']['fifth']['Most Frequently edited POI'] = ob.filter(Q(user=user) & \
+					Q(timestamp__date__range=[start,end]) & Q(feature_type='node')).raw('''SELECT k, v, count(*) \
+					as count FROM ( SELECT skeys(tags) AS k, svals(tags) \
+					as v, user, timestamp FROM populate_feature) AS t \
+					WHERE k='amenity' GROUP BY k, v ORDER BY count DESC LIMIT 1''')
 
-		d.deprint(stat['Nodes']['user'])	# DEBUG
+				break
 
 	# user search ways
 	if user != "" and not foundways:
-        	stat['Ways']['user']['OSM Username'] = user
+
 		foundwr = False
 		for index, item in enumerate(ws):
 			if item[index][0] == user:
-				stat['Ways']['user']['rank'] = index
-				foundwr = True
+				stat['Ways']['fifth'] = {}
+				stat['Ways']['fifth']['rank'] = index
+				stat['Ways']['fifth']['OSM Username'] = user
+				stat['Ways']['fifth']['Highlighted'] = 1
+				stat['Ways']['fifth']['Most Frequently edited POI'] = ob.filter(Q(user=user) & \
+					Q(timestamp__date__range=[start,end]) & \
+					Q(feature_type='way')).raw('''SELECT k, v, count(*) \
+					as count FROM ( SELECT skeys(tags) AS k, svals(tags) \
+					as v, user, timestamp FROM populate_feature) AS t \
+					WHERE k='amenity' GROUP BY k, v ORDER BY count DESC LIMIT 1''')
 				break
-		if not foundwr:
-			stat['Ways']['user']['rank'] = 0
-		stat['Ways']['user']['highlighted'] = True
-		stat['Ways']['user']['Most Frequently edited POI'] = ob.filter(Q(user=user) & \
-		Q(timestamp__date__range=[start,end]) & \
-		Q(feature_type='way')).raw('''SELECT k, v, count(*) \
-			as count FROM ( SELECT skeys(tags) AS k, svals(tags) \
-			as v, user, timestamp FROM populate_feature) AS t \
-			WHERE k='amenity' GROUP BY k, v ORDER BY count DESC LIMIT 1''')
-
-		d.deprint(stat['Ways']['user'])	# DEBUG
 
 	d.deprint(stat)	# DEBUG
 	d.deend()
