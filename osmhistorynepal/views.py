158467251dc63f2b671cb514f4d1db266951ea46
Max von Hippel <maxvonhippel1996@gmail.com>
2016-11-20 10:36:12 +0545

    as an experiment let's try not using a | b in the pre queryset ob

--- osmhistorynepal/views.py
+++ osmhistorynepal/views.py
@@ -119,11 +119,11 @@ def selection_statistics_view(request, range, mn_x, mn_y, mx_x, mx_y, user):
 	# define our bounding box
 	box = Polygon.from_bbox((mn_x, mn_y, mx_x, mx_y))
 	# get all the objects
-	ndtmp = Feature.geoobjects.filter(Q(point__intersects=box) & Q(feature_type='node'))
+	ndtmp = Feature.geoobjects.filter(Q(feature_type='node') & Q(point__intersects=box))
 	# get the unique ids from ndtmp as strings
 	strids = ndtmp.extra({'feature_id_str':"CAST(feature_id AS VARCHAR)"}).values_list('feature_id_str',flat=True).distinct()
 	# combine all features containing >=1 ok members with my existing list of ok nodes
-	ob = Feature.geoobjects.filter(members__ref__in=strids) | ndtmp
+	ob = Feature.geoobjects.filter(Q(members__ref__in=strids) | (Q(feature_type='node') & Q(point__intersects=box)))
 	# for more, see:
 	# http://stackoverflow.com/questions/40585055/querying-objects-using-attribute-of-member-of-many-to-many/40602515#40602515
 
