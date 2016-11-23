13aeb8b0c8e0cacf3877b8656733fec0d917fb67
maxvonhippel <maxvonhippel1996@gmail.com>
2016-11-23 15:23:25 +0545

    currently serves anyone on local network, eventually will need to add domain name and proxy and edit allowed hosts in settings.py so we can serve any client on web

--- geodjango/settings.py
+++ geodjango/settings.py
@@ -14,7 +14,8 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
@@ -23,10 +24,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'lje$we7!g5=vo)wp8v-1&995u*0jh#4wwcs!dqx3=-e8zg0$oo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ '*' ] # eventually replace to only allow client app
 
+# USE_X_FORWARDED_HOST = True
+# USE_X_FORWARDED_PORT = True
 
 # Application definition
 
@@ -134,9 +137,6 @@ USE_L10N = True
 USE_TZ = True
 
 INTERNAL_IPS = '127.0.0.1'
-
-DEBUG = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
